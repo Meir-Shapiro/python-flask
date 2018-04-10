82ccc1f3c5e229ca79e3a7550454fc9a04f35aee
Meir-Shapiro <meirs@ltccs.com>
2018-04-10 15:19:06 -0400

    Chapter 5.5

--- app/models.py
+++ app/models.py
@@ -1,13 +1,22 @@
 from datetime import datetime
 from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), index=True, unique=True)
     email = db.Column(db.String(100), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User: {} Email: {}>'.format(self.username, self.email)
@@ -21,3 +30,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post: {} Author: {}>'.format(self.body, self.user_id)
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
