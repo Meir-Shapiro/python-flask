d40b7e54f515ffdc16a48b219ad8aec3092ff033
Meir-Shapiro <meirs@ltccs.com>
2018-04-16 12:14:27 -0400

    Submission & displaying of blog posts.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,12 @@
-from flask import render_template, flash, redirect, url_for, request, current_app
+from flask import render_template, flash, redirect, url_for, request, current_app, jsonify
 from app import db
 from app.main import bp
-from app.main.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.main.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.main.models import User
+from app.main.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
+from guess_language import guess_language
 
 
 @bp.before_request
@@ -15,21 +16,23 @@ def before_request():
         db.session.commit()
 
 
-@bp.route('/')
-@bp.route('/index')
+@bp.route('/', methods=['POST', 'GET'])
+@bp.route('/index', methods=['POST', 'GET'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('main/index.html', title='Home Page', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+
+        post = Post(body=form.post.data, author=current_user, language=language)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('main.index'))
+    posts = current_user.followed_posts().all()
+    return render_template('main/index.html', title='Home Page', posts=posts, form=form)
 
 
 @bp.route('/login', methods=['POST', 'GET'])
@@ -75,10 +78,7 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    posts = user.posts
     return render_template('main/user.html', user=user, posts=posts)
 
 
@@ -134,3 +134,10 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('main.user', username=username))
+
+
+@bp.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('main/index.html', title='Explore', posts=posts)
